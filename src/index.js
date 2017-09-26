857c19b5d189b8fe4b3e820ecebb580e53ea7696
Tatyana Kahanovich <d-m-tanya@yandex.ru>
2017-09-26 20:26:29 +0300

    result Brackes not all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	str = str.split("");
+	var len = str.length;
+	var arr = [];
+	for (var i = 0; i < len; i++){
+		for (var j = 0; j < bracketsConfig.length; j++){
+			if(str[i] == bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1]){
+				arr.push(str[i]);
+			}
+			if(str[i] == bracketsConfig[j][1] && arr[arr.length-1] == bracketsConfig[j][0] && i > 0){
+				arr.pop();
+			}
+		}
+	}
+	return arr.length === 0 ? true : false;
 }
