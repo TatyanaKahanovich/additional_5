1e53d49e4229610342028b8fd0d82d678cdc655a
Tatyana Kahanovich <d-m-tanya@yandex.ru>
2017-09-29 13:55:24 +0300

    Brackets all solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
 	str = str.split("");
 	var len = str.length;
+
 	var arr = [];
 	for (var i = 0; i < len; i++){
 		for (var j = 0; j < bracketsConfig.length; j++){
-			if(str[i] == bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1]){
-				arr.push(str[i]);
-			}
-			if(str[i] == bracketsConfig[j][1] && arr[arr.length-1] == bracketsConfig[j][0] && i > 0){
+			if(str[i] == bracketsConfig[j][1] && arr[arr.length - 1] == bracketsConfig[j][0]){
 				arr.pop();
+			} else if(str[i] == bracketsConfig[j][0]){
+				arr.push(str[i]);
 			}
 		}
 	}
-	return arr.length === 0 ? true : false;
+	return arr.length == 0 &&  len % 2 == 0 ?  true : false;
 }
