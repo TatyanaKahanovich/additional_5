c763b0f9d92fc543219edfad0d991ffcd0f1b799
Tatyana Kahanovich <d-m-tanya@yandex.ru>
2017-09-30 18:21:36 +0300

    clear code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
 	str = str.split("");
-	var len = str.length;
+	let len = str.length,
+	    arr = [];
 
-	var arr = [];
-	for (var i = 0; i < len; i++){
-		for (var j = 0; j < bracketsConfig.length; j++){
+	for (let i = 0; i < len; i++){
+		for (let j = 0; j < bracketsConfig.length; j++){
 			if(str[i] == bracketsConfig[j][1] && arr[arr.length - 1] == bracketsConfig[j][0]){
 				arr.pop();
 			} else if(str[i] == bracketsConfig[j][0]){
